9d6285ede495f8918b7031570e22c0e417145566
FlashGordon95 <ryangordon210@gmail.com>
2016-10-05 09:41:21 +0100

    Testing flask functions and routes

--- problemSheets/flask/hello.py
+++ problemSheets/flask/hello.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/name")
+def name():
+    return "Hello Ryan!"
+
+@app.route("/name/<user>")
+def helloName(user):
+    return "Hello Student {}".format(user)
+
 if __name__ == "__main__":
     app.run()
+
