233ff17bb0df7e763c961c61a123eca398ab6db0
FlashGordon95 <ryangordon210@gmail.com>
2016-10-05 09:58:29 +0100

    Take in input of name and route to a page where shows name

--- problemSheets/flask/hello.py
+++ problemSheets/flask/hello.py
@@ -1,18 +1,28 @@
 from flask import Flask
 app = Flask(__name__)
 
+# Hello
 @app.route("/")
 def hello():
     return "Hello World!"
 
+# Simple hello function
 @app.route("/name")
 def name():
     return "Hello Ryan!"
 
+# Route takes input based on URL provide /name/Ryan provides the param user
 @app.route("/name/<user>")
 def helloName(user):
     return "Hello Student {}".format(user)
 
+@app.route("/signup")
+def signup():
+    email = request.form['email']
+    print("The email address is '" + email + "'")
+
+# This statement must be after the route definitions
+# Will cause a breakpoint, or will not render anything below the snippet
 if __name__ == "__main__":
     app.run()
 
