4577f7a89e2ee0c3042c4c0606f2ac0d3f802867
FlashGordon95 <ryangordon210@gmail.com>
2016-10-12 10:18:31 +0100

    Testing jinja and template rendering

--- problemSheets/flask/hello.py
+++ problemSheets/flask/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # Hello
@@ -20,6 +20,26 @@ def helloName(user):
 def signup():
     email = request.form['email']
     print("The email address is '" + email + "'")
+
+# Loads a page from the templates folder called index.html
+@app.route("/<string:page_name>/")
+def template_page(page_name):
+    return render_template('%s.html' % page_name)
+
+@app.route("/")
+def static_page():
+    return current_app.send_static_file('index.html')
+
+@app.route('/student')
+def student():
+   return render_template('student.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
+
 
 # This statement must be after the route definitions
 # Will cause a breakpoint, or will not render anything below the snippet
